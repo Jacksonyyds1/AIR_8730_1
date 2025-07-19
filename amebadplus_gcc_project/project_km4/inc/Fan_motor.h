7cf387f1f26b7efa2434df6bf373a61af2be3485
Jacksonyyds1 <jackson_cheng@intretech.com>
2025-07-18 21:45:13 -0400

    add pwm timer9 capture timer 8

--- amebadplus_gcc_project/project_km4/inc/Fan_motor.h
+++ amebadplus_gcc_project/project_km4/inc/Fan_motor.h
@@ -7,19 +7,13 @@
 // 风扇配置参数
 #define FAN_FG_PULSE_PER_CYCLE  12    // 每转脉冲数
 #define FAN_PWM_FREQ           4000   // PWM频率 4kHz
-#define FAN_PWM_MAX_DUTY       1000   // 最大占空比
+#define FAN_PWM_MAX_DUTY       4000   // 最大占空比
 #define PID_OUTPUT_BIAS        600    // PID输出偏置
 #define PID_KP                 0.08f   // PID参数
 #define PID_KI                 0.02f
 #define PID_KD                 0.08f
 #define PID_OUTPUT_MAX         (4000 - PID_OUTPUT_BIAS)
 #define PID_OUTPUT_MIN         (0 - PID_OUTPUT_BIAS)
-
-// GPIO和定时器配置
-#define FAN_PWM_PIN           _PB_30   // PWM输出引脚
-#define FAN_FG_PIN            _PB_31    // FG信号输入引脚
-#define FAN_PWM_TIMER_IDX     4        // 使用Timer4作为PWM
-#define FAN_FG_TIMER_IDX      8        // 使用Timer8作为FG捕获
 
 // 函数声明
 void fan_speed_controller_init(const int *table, int size);
@@ -33,4 +27,7 @@ int fan_speed_get_current_duty(void);
 
 void fan_controller_example(void);
 void test_pwm_output(void);
+
+void timer9_pwm_set_duty(float duty_percent);
+void timer9_pwm_init(void);
 #endif // __FAN_MOTOR_H
